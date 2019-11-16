6c621992a89975fc05d9d57d712397c9aa00d6f8
Timothy Karani <kartimothy@gmail.com>
2019-11-16 06:34:19 +0300

    Added SMTP Password resetting

--- university_portal/settings.py
+++ university_portal/settings.py
@@ -144,4 +144,9 @@ LOGIN_URL = '/users/login/'
 # Since we are using bootstrap4
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
